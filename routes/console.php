7851117bd706e3b5fe3e7a61aa7fa1df497592a3
Ygor Fernandes <ygorvieirafernades@gmail.com>
2026-05-21 19:43:07 -0300

    Alterando o tempo que ele tem que rodar o robô todo dia

--- routes/console.php
+++ routes/console.php
@@ -2,11 +2,10 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-use Illuminate\Support\Facades\Schedule;
 
-// Roda o robô de raspagem de preços todos os dias às 10 da manhã
-Schedule::command('precos:verificar')->dailyAt('10:00');
+Schedule::command('precos:verificar')->dailyAt('19:50')->timezone('America/Sao_Paulo');;
